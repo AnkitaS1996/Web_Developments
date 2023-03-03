94b8ffc62dcfe5ea07690dadbd691568be84e09f
AnkitaS1996 <ankitashedage2661@gmail.com>
2023-03-04 01:34:30 +0530

    Assignment No 08 Upload

---
+++ Assignments/Assignment_No_8/Example_01.js
@@ -0,0 +1,10 @@
+var No = 5;
+
+if(No % 2 == 0)
+{
+    console.log("Number is Even");
+}
+else
+{
+    console.log({No} + "Number is Odd ");
+}
---
+++ Assignments/Assignment_No_8/Example_05.js
@@ -0,0 +1,7 @@
+var ArrNum = [21,10,56,20,10,56,10,95,23,57,10,11,10];
+var Count = 0;
+for(var i = 1;i<=ArrNum.length;i++)
+{
+     Count++;
+}
+console.log(Count);
